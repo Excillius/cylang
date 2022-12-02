bbbd714d8b71573b0e0c5703cc2036fa7d768a8f
eksupi <david.csz00@gmail.com>
2022-12-02 22:22:10 +0700

    Update update.php

--- src/php/update.php
+++ src/php/update.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     $conn = mysqli_connect('cylang_db', 'cylang', '7:2,M4AytU:Rf7', 'logindb');
     
     $username = mysqli_real_escape_string($conn,$_POST['username']);
@@ -6,19 +7,17 @@
     $pass = mysqli_real_escape_string($conn,$_POST['newPass']);
     $address = mysqli_real_escape_string($conn,$_POST['address']);
 
-    $koki = $_COOKIE["session"];
-    $update_users = "UPDATE users SET username = '$username', email = '$email', pass = '$pass', domicile='$address' WHERE hashing='$koki'";
+    $id = $_SESSION["id"];
+    $update_users = "UPDATE users SET username = '$username', email = '$email', pass = '$pass', domicile='$address' WHERE id='$id'";
     mysqli_query($conn, $update_users);
 
-    $mysql_get_users = "SELECT * FROM users WHERE hashing='$koki'";
+    $mysql_get_users = "SELECT * FROM users WHERE id='$id'";
     $get_rows = mysqli_query($conn, $mysql_get_users);
 
     if($row = mysqli_fetch_assoc($get_rows))
     {
-        $sess = hash('sha256', $email);
-        setcookie('session', $sess, time() + (60 * 60), '/');
-        $update_users = "UPDATE users SET hashing='$sess' WHERE email='$email'";
-        mysqli_query($conn, $update_users);
+        $_SESSION["username"] = $row["username"];
+        $_SESSION["email"] = $row["email"];
         echo "<script>alert('You have successfully updated your profile!!');window.location = 'http://0.0.0.0:3738/profile.php';</script>";
     }
 ?>
