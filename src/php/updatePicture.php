44b846ac079583195080a0cfeab1834462d42efc
Delbert Giovanni Lie <degiodelbert.095@gmail.com>
2022-12-06 02:05:42 +0700

    revision on updatePicture.php

--- src/php/updatePicture.php
+++ src/php/updatePicture.php
@@ -4,74 +4,85 @@ if (!file_exists($upload_dir)) {
     mkdir($upload_dir, 0777, true);
 }
 
-// create an error message
-$error = "";
+// check if upload is empty
+if ($_FILES['file']['error'] === 4)
+{
+    echo "<script>alert('File must not be empty');window.location = '../update.php'</script>";
+    exit(); 
+}
 
-// get file name
+// get file details
 $uploaded_file = $_FILES["file"];
 $uploaded_file_name = $_FILES["file"]["name"];
 $uploaded_file_size = $_FILES["file"]["size"];
-$uploaded_file_temp_directory = $_FILES["file"]["tmp_name"];
+$uploaded_file_temp_name = $_FILES["file"]["tmp_name"];
+
+validateExtension($uploaded_file_name);
+validateFileSize($uploaded_file_size);
+$newFile = moveAndRenameFile($uploaded_file_temp_name, $upload_dir, $uploaded_file_name);
+validateHeader($newFile);
 
 // function validate file extension (file type and more than one extension)
-function validateExtension($uploaded_file_name, $error) {
+function validateExtension($uploaded_file_name) {
     $file_array = explode(".", $uploaded_file_name);
-
-    if (count($file_array) == 2) { // jika tipe data hanya 1
-        $temp_file_type = strtolower($file_array[1]);
-
-        if ($temp_file_type != "jpg" && $temp_file_type != "png" && $temp_file_type != "jpeg") {
-            $error = "This extension is not allowed!";
-        }
-    }
-    else { // jika tipe data lebih dari 1 atau kurang dari 1
-        $error = "There was an error uploading your file.";
+   
+    if (count($file_array) > 2) { // jika terdapat lebih dari satu extension
+        if ($file_array[1] !== "jpg" or $file_array[1] !== "png" or $file_array[1] !== "jpeg"){
+            echo "<script>alert('This extension is not allowed!');window.location = '../update.php'</script>";
+            exit();
+        }    
     }
 }
 
 // function validate file size (max 1MB)
-function validateFileSize($uploaded_file_size, $error) {
+function validateFileSize($uploaded_file_size) {
     if ($uploaded_file_size > 1000000) {
-        $error = "File must be 1MB or less in size.";
+        echo "<script>alert('File must be 1MB or less in size.');window.location = '../update.php'</script>";
+        exit();
     }
 }
 
-function validateHeader($uploaded_file, $error){
-    if ( !file_exists( $uploaded_file ) ) return false;
-    if ( $f = fopen($uploaded_file, 'rb') ) 
+//function for move and renaming the file
+function moveAndRenameFile($tmp, $upload_location, $file_name){
+    $file_id = uniqid();
+    $new_rand_file_name = md5($file_id) . "_" . time() . "_" . basename($file_name);
+    $base_file = $upload_location . $new_rand_file_name;
+
+    if (move_uploaded_file($tmp, $base_file)) {
+        return $base_file;
+        exit();
+    }
+    else {
+        echo "<script>alert('There was an error uploading your file.');window.location = '../update.php'</script>";
+        exit();
+    }   
+}
+
+//function to check file by it's signature header
+function validateHeader($file){
+    if ( $f = fopen($file, 'rb') ) 
     {
         $header = fread($f, 8);
+        $header = strtolower($header);
         fclose($f);
-    // Signature = PNG & JPG
-        if(!((strncmp($header, "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a", 8)==0 && strlen ($header)==8) ||(strncmp($header, "\xff\xd8\xff", 3)==0 && strlen ($header)==3)) )
+
+        // check file signature must be PNG or JPG/JPEG
+        if( strncmp($header, "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a", 8) != 0 )
         {
-            $error = "Error: File format is not PNG or JPG!";
+            $f = fopen($file, 'rb');
+            $header = fread($f, 3);
+            $header = strtolower($header);
+            fclose($f);
+            
+            if ( strncmp($header, "\xff\xd8\xff", 3) != 0 )
+            {
+                unlink($file);
+                echo "<script>alert('File format is not PNG or JPG/JPEG!');window.location = '../update.php'</script>";
+                exit();
+            } 
         }
     } 
 }
 
-// rename file
-$new_rand_file_name = "";
-function randomizeFileName($uploaded_file_name, $new_rand_file_name) {
-    $file_hash = uniqid();
-    $new_rand_file_name = md5("$file_hash") . "_" . time() . "_" . basename($uploaded_file_name);
-}
-
-validateFileSize($uploaded_file_size, $error);
-validateExtension($uploaded_file_name, $error);
-validateHeader($uploaded_file);
-randomizeFileName($uploaded_file_name, $new_rand_file_name);
-
-$upload_file_to = $upload_dir . $new_rand_file_name;
-if (empty($error)) { // if no found error (validated and OK)
-    if (move_uploaded_file($uploaded_file_temp_directory, $upload_file_to)) {
-        echo "You have successfully updated your profile picture!";
-    }
-    else {
-        echo "Error: There was an error uploading your file.";
-    }
-}
-else {
-    echo "Error: " . $error;
-}
+echo "<script>alert('You have successfully updated your profile picture!');window.location = '../update.php'</script>";
 ?>
