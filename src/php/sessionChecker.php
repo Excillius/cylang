8da60cda740a2c8426a3aa1fe37ba9e846155926
Excillius <clintswood@binus.ac.id>
2022-12-06 14:37:19 +0700

    Update sessionChecker.php

--- src/php/sessionChecker.php
+++ src/php/sessionChecker.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if(isset($_SESSION['id']) && empty($_SESSION['id']) === true){
+    if(!isset($_SESSION['id']) && empty($_SESSION['id']) === true){
     echo "<script>alert('Please create an account or login to an account first!!');window.location = 'http://0.0.0.0:3738/login.html';</script>";
     }
 ?>
